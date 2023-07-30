65e0d18b3f87a3122e74eacdb5d7ebb947ae9802
Burak Soykan <bsoykan@k2m12s07.42kocaeli.com.tr>
2023-07-30 16:04:58 +0300

    k

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsoykan <bsoykan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 09:19:54 by mecatalk          #+#    #+#             */
+/*   Updated: 2023/07/30 15:02:12 by bsoykan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*read_function(int fd, char *str);
+char	*ft_getline(char *fullStr);
+char	*ft_getrest(char *fullStr);
+
+#endif
---
+++ test.c
@@ -0,0 +1,19 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+int main()
+{
+    int fd = open("test.txt", O_RDONLY);
+    char *line;
+    int i = 0;
+    while ((line = get_next_line(fd)))
+    {
+        printf("%d: %s\n", i, line);
+        free(line);
+        i++;
+    }    
+
+    close(fd);
+    return (0);
+
+}
